bb72b167d05b5b4a11b62f0e26f632710e553f3d
altaf Sheikh <defaultcompny0@gmail.com>
2026-04-09 20:22:28 +0530

    Added: project images

--- portfolio/src/components/layout/model/model.tsx
+++ portfolio/src/components/layout/model/model.tsx
@@ -48,13 +48,14 @@ const SubNavBar = () => {
 
     return (
         <div onClick={() => setComponent({ type: 'null' })} style={{ display: component.type === 'null' ? 'none' : 'block', }} className='Model'>
-            <div style={{}}>
+
+
+
+            <div onClick={(e) => e.stopPropagation()} className='Model-Content'>
                 <svg className='subnavbar-close' onClick={() => setComponent({ type: 'null' })} xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" viewBox="0 0 16 16">
                     <path d="M4.646 4.646a.5.5 0 0 1 .708 0L8 7.293l2.646-2.647a.5.5 0 0 1 .708.708L8.707 8l2.647 2.646a.5.5 0 0 1-.708.708L8 8.707l-2.646 2.647a.5.5 0 0 1-.708-.708L7.293 8 4.646 5.354a.5.5 0 0 1 0-.708" />
                 </svg>
-            </div>
-            <div onClick={(e) => e.stopPropagation()} className='Model-Content'>
-                    {renderComponent()}
+                {renderComponent()}
             </div>
         </div>
     )
--- portfolio/src/components/layout/project/projectdata.ts
+++ portfolio/src/components/layout/project/projectdata.ts
@@ -1,9 +1,13 @@
-import img from '../../../assets/Frame 1 (4).png'
+
 import imgs from '../../../assets/Frame 1.webp'
 import thumbnail from '../../../assets/Frame 2 (1).webp'
+import thumbnail1 from '../../../assets/Frame 7.webp'
+import thumbnail2 from '../../../assets/Frame 6 (1).webp'
+import img1 from '../../../assets/image 1.png'
+import img2 from '../../../assets/image 4.png'
 const ProjectData = [
     {
-        imgs: [{ url: imgs }, { url: img }],
+        imgs: [{ url: imgs }, { url: imgs }],
         thumbnail: thumbnail,
         category: "Software",
         heading: "A full stack datalake software design for non technical user",
@@ -12,7 +16,34 @@ const ProjectData = [
     },
 
     {
-        imgs: [{ url: imgs }, { url: img }],
+        imgs: [{ url: img1 }, { url: imgs }],
+        thumbnail: thumbnail1,
+        category: "Website",
+        heading: "A full stack datalake software design for non technical user",
+        description: "hello world",
+        budget: `$996 - $1200`
+    },
+
+    {
+        imgs: [{ url: img2 }, { url: imgs }],
+        thumbnail: thumbnail2,
+        category: "Automation",
+        heading: "A full stack datalake software design for non technical user",
+        description: "hello world",
+        budget: `$996 - $1200`
+    },
+
+    {
+        imgs: [{ url: imgs }, { url: imgs }],
+        thumbnail: thumbnail,
+        category: "Automation",
+        heading: "A full stack datalake software design for non technical user",
+        description: "hello world",
+        budget: `$996 - $1200`
+    },
+
+    {
+        imgs: [{ url: imgs }, { url: imgs }],
         thumbnail: thumbnail,
         category: "Software",
         heading: "A full stack datalake software design for non technical user",
@@ -21,34 +52,7 @@ const ProjectData = [
     },
 
     {
-        imgs: [{ url: imgs }, { url: img }],
-        thumbnail: thumbnail,
-        category: "Software",
-        heading: "A full stack datalake software design for non technical user",
-        description: "hello world",
-        budget: `$996 - $1200`
-    },
-
-    {
-        imgs: [{ url: imgs }, { url: img }],
-        thumbnail: thumbnail,
-        category: "Software",
-        heading: "A full stack datalake software design for non technical user",
-        description: "hello world",
-        budget: `$996 - $1200`
-    },
-
-    {
-        imgs: [{ url: imgs }, { url: img }],
-        thumbnail: thumbnail,
-        category: "Software",
-        heading: "A full stack datalake software design for non technical user",
-        description: "hello world",
-        budget: `$996 - $1200`
-    },
-
-    {
-        imgs: [{ url: imgs }, { url: img }],
+        imgs: [{ url: imgs }, { url: imgs }],
         thumbnail: thumbnail,
         category: "Software",
         heading: "A full stack datalake software design for non technical user",
